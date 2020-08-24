88c8646c1bd2dd0f2e3cb7a4cf2b0d69d0dff15f
SyedKaz00 <skaz269@aucklanduni.ac.nz>
2020-08-25 11:59:16 +1200

    Updates

---
+++ precache-manifest.711ab44b48ec69db8bae4ae778d7dfa0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac4791d1eeb23481fcaf2251e218138e",
+    "url": "/film/index.html"
+  },
+  {
+    "revision": "5fb77f42245942c081cf",
+    "url": "/film/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5bddc87a5e6ad58a8748",
+    "url": "/film/static/js/2.3c73fe3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/film/static/js/2.3c73fe3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fb77f42245942c081cf",
+    "url": "/film/static/js/main.79f109d5.chunk.js"
+  },
+  {
+    "revision": "0d615abfb87ef833a4a8",
+    "url": "/film/static/js/runtime-main.8ae3abee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/film/static/media/logo.5d5d9eef.svg"
+  }
+]);
